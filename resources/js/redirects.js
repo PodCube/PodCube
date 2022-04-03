a58b7ab08b3691c7e8e08046e749342eb5258760
PodCube <github@poweredbypodcube.com>
2022-04-03 12:31:33 -0400

    restructure everything

---
+++ resources/js/redirects.js
@@ -0,0 +1,24 @@
+var RedirectList = {
+
+    file:
+    {
+        "039" : "https://pnc.st/s/pc/bd5eb302/fort-smith-ice-skating-rink-snowballs-jr-hockey-04-04-2051"
+    },
+
+    news:
+    {
+        "001": "https://www.getrevue.co/profile/podcube/issues/march-at-podcube-hq-1081247"
+    },
+
+    page:
+    {
+        "facebook":"https://facebook.com/podcube",
+        "youtube":"https://www.youtube.com/channel/UCjo7q-qSHg_8Zo8ln-W1X8w",
+        "twitter":"https://twitter.com/podcube",
+        "tiktok":"https://tiktok.com/@podcube",
+        "reddit":"https://reddit.com/user/PodCube",
+        "donate":"https://www.paypal.com/donate/?hosted_button_id=P93ZFPQW5MY9A"
+    }
+}
+
+var DefaultLanding = "https://pod.link/podcube"
